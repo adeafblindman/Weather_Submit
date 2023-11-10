b0ed77e1ee2568429a5d085cdf6833e24783f365
adeafblindman <adeafblindman@gmail.com>
2023-11-09 21:41:29 -0600

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1nZR543Dekzy_7fpX6oUgXFnQD2WTOPCdngZ14i0GtFg/edit#gid=164271551';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyAxQ6nWu_n2d5Xk2YQ5muiaLDPxvN4jCzI';
